8c25ab3d1ee05761037267a7e4b6e79672d4cf3e
arakiyuna <arakiyuna@192.168.0.13>
2020-05-15 16:50:18 +0900

    hanami

--- FavoriteFood/src/Main.java
+++ FavoriteFood/src/Main.java
@@ -4,9 +4,9 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		String s = "私の好きな食べ物はアイスです。";
+		String s1 = "私の好きな食べ物はアイスです。";
 		
-		System.out.println(s);
+		System.out.println(s1);
 	}
 
 }
